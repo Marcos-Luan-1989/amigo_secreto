d02ac1197170a0ba7390cd05656bd11752dc56b0
Marcos Luan <marcos.luan_estudos@hotmail.com>
2025-02-10 18:49:20 -0300

    Incluindo a funcionalidade do botão sortear (função sortearAmigo() )

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 //criando a lista para receber os nomes dos amigos participantes
 let listaDeAmigos = [];
+let numerosJaSorteados = [];
 
 function adicionarAmigo(){
     //criando a variavel para receber o texto digitado.
@@ -11,9 +12,9 @@ function adicionarAmigo(){
         alert("Por favor, insira um nome.");
     } else{
         listaDeAmigos.push(nomeDoAmigo);
-        console.log(`${nomeDoAmigo} inserido a lista.`);
+        // console.log(`${nomeDoAmigo} inserido a lista.`);
         limparCampoNome();        
-        console.log(listaDeAmigos);
+        // console.log(listaDeAmigos);
         atualizarListaDeAmigosHtml();
     }
 
@@ -26,6 +27,8 @@ function limparCampoNome (){
 
 function sortearAmigo(){
     atualizarListaDeAmigosHtml();
+    atualizarResultado();
+
 
 }
 
@@ -51,4 +54,62 @@ function atualizarListaDeAmigosHtml (){
 
     }
 
+}
+
+function gerarNumeroAleatorio (){
+    let numeroAleatorio = parseInt(Math.random()* listaDeAmigos.length);
+
+    let quantidadeDePessoasSorteadas = numerosJaSorteados.length;
+    if (listaDeAmigos.length < 2) {
+        return "";
+
+    } else {
+
+        //verificar se o número aleatório já foi sorteado.
+        if (quantidadeDePessoasSorteadas == listaDeAmigos.length ){
+            alert("Todas as pessoas já foram sorteadas, favor gerar uma nova lista de participantes!!!");
+        } else {
+
+            if (numerosJaSorteados.includes(numeroAleatorio)) {
+
+                return gerarNumeroAleatorio();
+            } else {
+                numerosJaSorteados.push(numeroAleatorio);
+
+                // testes via console para verificar o funcionamento.
+                // console.log(`O número aleatório gerado é ${numeroAleatorio}`);
+                // console.log(`A pessoa sorteada foi: ${listaDeAmigos[numeroAleatorio]}`);
+                // console.log(`Esses são os números já sorteados: ${numerosJaSorteados}`);
+                
+                return listaDeAmigos[numeroAleatorio];
+            }
+
+        }
+        
+    }
+    
+}
+
+function atualizarResultado() {
+    let amigoSorteado = gerarNumeroAleatorio();
+    if (amigoSorteado == "") {
+        
+        return alert("Por favor insira o nome de pelo menos 2 pessoas para realizar o sorteio.")
+
+    } else {
+
+            //recebendo o elemento lista<ul> dentro do javascript
+    let listaHtml = document.getElementById("listaAmigos");
+
+    //limpando a lista para garantir que não haverão duplicatas.
+    listaHtml.innerHTML = "";
+
+            let resultadoSorteio = document.getElementById("resultado");
+            resultadoSorteio.innerHTML = "";
+
+            let mostrarResultado = document.createElement("li");
+            mostrarResultado.textContent = amigoSorteado;
+            resultadoSorteio.appendChild(mostrarResultado);
+    
+        }
 }
